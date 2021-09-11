3ca34c4530197702dd34871f3b045709959ab57d
Varun Lanjhara <lanjharav@gmail.com>
2021-09-11 15:09:04 +0530

    Bugs Fixed

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path,include
 from main import views
 from main.views import AddFollower, PasswordResetVieww, PostListView, RemoveFollower, SavePost, update_profile,AddDislike,AddLikes
-from django.contrib.auth.views import PasswordResetDoneView, PasswordResetView,PasswordResetConfirmView,PasswordResetCompleteView
 from main.views import PostCreateView,PostDetailview,PostUpdateView,PostDeleteView
 urlpatterns = [
-    path('password_reset/',PasswordResetVieww.as_view(),name = "password_reset"),
-    path('password_reset/done/',PasswordResetDoneView.as_view(template_name = "reset/password_reset_done.html"),name = "password_reset_done"),
-    path('password_reset_confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name = "reset/password_reset_confirm.html"),name = "password_reset_confirm"),
-    path('password_reset/complete/',PasswordResetCompleteView.as_view(template_name = "reset/password_reset_complete.html"),name = "password_reset_complete"),
     path('login/',views.loginUser,name = "login"),
     path('register/',views.registerUser,name = "register"),
     path('logout/',views.logoutUser,name = "logout"),
